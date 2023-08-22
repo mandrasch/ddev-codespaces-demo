3aea7b480d6537a19c4af9f008fac1eb1b43b39e
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-08-22 06:35:52 +0200

    chore: add setup_project.sh

---
+++ .devcontainer/setup_project.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+# download images beforehand
+ddev debug download-images
+
+# avoid errors on rebuilds
+ddev poweroff
+
+# start ddev project
+ddev start -y
+
+# further install steps 
+ddev composer install 
