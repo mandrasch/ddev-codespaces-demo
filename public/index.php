ce12e9f785a3a6b5e6a729968e80f88cca6adf94
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2023-08-22 06:41:06 +0200

    chore: add demo php file

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello Codespaces!</title>
+
+  <style type="text/css">
+    h1{font-family: sans-serif;}  
+  </style>
+</head>
+
+<body>
+
+  <h1><?php echo "Hello, codespaces!" ?><h1>
+
+</body>
+</html>
